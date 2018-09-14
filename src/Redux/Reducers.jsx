f77ca81c33d33a1d930e2677f23d0915b4eadf4d
Patrick Hanson <patrickhanson08@gmail.com>
2018-09-14 14:35:11 -0400

    set up reducer file

--- src/Redux/Reducers.jsx
+++ src/Redux/Reducers.jsx
@@ -1,1 +1,16 @@
-import Types from './Types'
+import {
+    LOGIN,
+    LOGOUT,
+    REGISTER,
+    BOOKSHELF,
+    BOOKS
+} from './Types'
+
+const initialState = {}
+
+export default function (state = initialState, action) {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
