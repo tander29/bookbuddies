79d6292c30735d3e5516a308198a985b0472a0ca
Travis Anderson <thetanderson@gmail.com>
2018-09-14 11:19:13 -0400

    setting up router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './App.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux'
+import store, { history } from './Redux/Store'
+import { ConnectedRouter } from 'connected-react-router'
+import { Route, Switch } from 'react-router'
+import HomePage from './Components/HomePage'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter basename={process.env.PUBLIC_URL} history={history}>
+            <Switch>
+                <Route exact path="/" render={(props) => (<HomePage location={props.location.pathname} />)} />
+
+
+            </Switch>
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root'));
 registerServiceWorker();
