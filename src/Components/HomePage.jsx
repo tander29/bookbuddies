90745628f7beba0f86989e624ced5b5c76a45613
Travis Anderson <thetanderson@gmail.com>
2018-09-17 15:25:20 -0400

    book shelf and book card update, have modal/close and send message function placeholder

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,11 +1,18 @@
+// when making a new component, every component needs to import react and connect, to properly make component and connect to redux store
 import React from "react";
 import { connect } from "react-redux";
+
+
+import BookShelf from './BookShelf.jsx'
 
 
 class HomePage extends React.Component {
     render() {
         return (
-            <div>Hi Everyone</div>
+            <React.Fragment>
+
+                <BookShelf></BookShelf>
+            </React.Fragment>
         );
     }
 }
