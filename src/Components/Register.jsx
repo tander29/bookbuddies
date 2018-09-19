a95daa13e8a02a3084e52dcd14e9e429e7ec9d15
Travis Anderson <thetanderson@gmail.com>
2018-09-19 14:21:32 -0400

    setting up the action to do a post request

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Card } from "semantic-ui-react";
 import { connect } from "react-redux";
+import { register } from '../Redux/ActLoginRegister'
 
 class Register extends Component {
     state = {
@@ -12,6 +13,15 @@ class Register extends Component {
 
     handleSubmit = () => {
         console.log("Hey, you clicked the Submit button!")
+        if (!this.state.password) { return }
+        console.log("the password field is not blank/false!")
+
+        if (this.state.password === this.state.reenterPassword) {
+            console.log("the password and reenter pass equal each other")
+            this.props.register(this.state.displayName, this.state.username, this.state.password)
+            return
+        }
+        console.log("!!!! the password and reenter pass do not equal each other")
     }
 
     updateDisplayName = (event) => {
@@ -94,7 +104,11 @@ const mapStateToProps = state => {
 };
 
 function mapDispatchToProps(dispatch) {
-    return {}
+    return {
+        register: (displayname, username, password) => {
+            dispatch(register(displayname, username, password))
+        }
+    }
 
 }
 const Connect = connect(
