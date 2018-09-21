0e32f1f6218ecaabf64f9bda7a0232b8e89ae5b8
Patrick Hanson <patrickhanson08@gmail.com>
2018-09-21 10:43:01 -0400

    added a skeleton to Main Page

--- src/Components/MessageBoard.jsx
+++ src/Components/MessageBoard.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Card } from "semantic-ui-react";
+import Messages from './Messages'
 
 class MessageBoard extends Component {
     state = {
         messages: []
     }
+
+    render() {
+        return(
+            <Messages />
+        )
+    }
 }
+
+export default MessageBoard
