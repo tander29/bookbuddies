90968deb04bdf0223bb6c65a2fb3cd51f8144b03
Patrick Hanson <patrickhanson08@gmail.com>
2018-09-24 14:42:10 -0400

    working on message component

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -6,6 +6,7 @@ class ErrorPage extends React.Component {
     render() {
         return (
             <React.Fragment>
+                <div>YOU DONE FUCKED UP</div>
                 <div>Nav bar import on this so we can navigate</div>
                 <div>Wrong page, go home</div>
             </React.Fragment>
--- src/Components/Messages.jsx
+++ src/Components/Messages.jsx
@@ -1,15 +1,41 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Card } from "semantic-ui-react";
+import { Card, Grid, Button, Modal, Divider } from "semantic-ui-react";
 
 class Messages extends Component {
+    handleEnter = (event) => {
+        if (event.key === 'Enter') {
+            console.log("Message sent...hopefully")
+        }
+    }
+    
     render() {
         return(
             <React.Fragment>
-                <Card className="messages" >
-                    <div>Name of Sender</div>
-                    <div>Time Stamp</div>
-                    <div>Message Content</div>
+                <Card className="messages" style={{ padding: '1vh', backgroundColor:'#474B4F' }} >
+                    <Grid>
+                        <Grid.Row columns={2} >
+                            <Grid.Column textAlign='left' >Message Sender</Grid.Column>
+                            <Grid.Column textAlign='right' >Time Stamp</Grid.Column>
+                        </Grid.Row>
+                        <Divider />
+                        <Grid.Row columns={2} >
+                            <Grid.Column textAlign='left' >Message Content</Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row columns={2}>
+                            <Grid.Column textAlign='left'></Grid.Column>
+                            <Grid.Column textAlign='right' >
+                                <Modal size='tiny' trigger={<Button style={{ backgroundColor:'#86C232', color: 'white' }} >Reply</Button>} closeIcon >
+                                    <div>Reply to Message Sender!</div>
+                                    <input 
+                                    autoFocus={true}
+                                    type='text'
+                                    onKeyDown={this.handleEnter}
+                                    />
+                                </Modal>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
                 </Card>
             </React.Fragment>
         )
