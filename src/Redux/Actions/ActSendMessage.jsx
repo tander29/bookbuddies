7348531f0d921c2e134b07b9312d0f28fe74f460
Travis Anderson <thetanderson@gmail.com>
2018-09-28 10:51:16 -0400

    downloading new things

--- src/Redux/Actions/ActSendMessage.jsx
+++ src/Redux/Actions/ActSendMessage.jsx
@@ -8,3 +8,12 @@ export const sendMessage = messageItem => dispatch => {
     body: { message: messageItem }
   };
 };
+
+export const getAllMessage = messageItem => dispatch => {
+  console.log("Action Message Sent:", messageItem);
+  const requestOptions = {
+    method: "POST",
+    headers: { "Application-Type": "application/json" },
+    body: { message: messageItem }
+  };
+};
